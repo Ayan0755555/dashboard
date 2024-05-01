096579b58772c3c9d5d9f2fab8394264cdd90534
ayan <mohdayanrehman07@gmail.com>
2024-05-01 19:54:03 +0530

    material-react-table-make

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,39 +1,17 @@
 "use client";
-import { useState } from "react";
+import React from "react";
 import Layout from "../components/layout";
 import Typography from "@mui/material/Typography";
 import UserList from "../users/userList";
-import UserDetails from "./userDetails";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export default function Analytics() {
-  const [addUser, setAddUser] = useState(false);
   return (
     <>
       <Layout>
-        {addUser ? (
-          <div>
-            <h2 class="sm:text-4xl text-2xl font-extrabold text-center mb-16">
-              Add User
-            </h2>
-            <ArrowBackIcon onClick={() => setAddUser(false)} />
-            <UserDetails />
-          </div>
-        ) : (
-          <div>
-            <h2 class="sm:text-4xl text-2xl font-extrabold text-center mb-16">
-              Users
-            </h2>
-            <button
-              type="button"
-              class="px-6 py-2 mt-4 mb-4 rounded text-black text-sm tracking-wider font-medium outline-none border-2 border-[#333] hover:bg-[#222] hover:text-white transition-all duration-300"
-              onClick={() => setAddUser(true)}
-            >
-              Add User
-            </button>
-            <UserList />
-          </div>
-        )}
+        <h2 class="sm:text-4xl text-2xl font-extrabold text-center mb-16">
+          Users List
+        </h2>
+        <UserList />
       </Layout>
     </>
   );
--- src/app/users/userList.jsx
+++ src/app/users/userList.jsx
@@ -1,21 +1,61 @@
 "use client";
-import * as React from "react";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
+import { useMemo, useState, useEffect } from "react";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
 import axios from "axios";
-import { useState, useEffect } from "react";
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
 
-export default function StickyHeadTable() {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+//nested data is ok, see accessorKeys in ColumnDef below
+// const data = [
+//   {
+//     name: {
+//       firstName: "John",
+//       lastName: "Doe",
+//     },
+//     address: "261 Erdman Ford",
+//     city: "East Daphne",
+//     state: "Kentucky",
+//   },
+//   {
+//     name: {
+//       firstName: "Jane",
+//       lastName: "Doe",
+//     },
+//     address: "769 Dominic Grove",
+//     city: "Columbus",
+//     state: "Ohio",
+//   },
+//   {
+//     name: {
+//       firstName: "Joe",
+//       lastName: "Doe",
+//     },
+//     address: "566 Brakus Inlet",
+//     city: "South Linda",
+//     state: "West Virginia",
+//   },
+//   {
+//     name: {
+//       firstName: "Kevin",
+//       lastName: "Vandy",
+//     },
+//     address: "722 Emie Stream",
+//     city: "Lincoln",
+//     state: "Nebraska",
+//   },
+//   {
+//     name: {
+//       firstName: "Joshua",
+//       lastName: "Rolluffs",
+//     },
+//     address: "32188 Larkin Turnpike",
+//     city: "Charleston",
+//     state: "South Carolina",
+//   },
+// ];
+
+const UsersList = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -34,89 +74,40 @@ export default function StickyHeadTable() {
         console.log(error, "this is error!!");
       });
   };
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
-  const deleteRecord = (row) => {
-    console.log("row",row)
-  };
-
-  const editRecord = (row) => {
-    console.log("editrow",row)
-  };
-  return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="right" style={{ minWidth: 70 }}>
-                ID
-              </TableCell>
-              <TableCell align="right" style={{ minWidth: 70 }}>
-                Name
-              </TableCell>
-              <TableCell align="right" style={{ minWidth: 70 }}>
-                Email
-              </TableCell>
-              <TableCell align="right" style={{ minWidth: 70 }}>
-                Password
-              </TableCell>
-              <TableCell align="center" style={{ minWidth: 70 }}>
-                Action
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row, index) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={data.code}>
-                    <TableCell key={index} align="right">
-                      {row.id}
-                    </TableCell>
-                    <TableCell key={index} align="right">
-                      {row.name}
-                    </TableCell>
-                    <TableCell key={index} align="right">
-                      {row.Email}
-                    </TableCell>
-                    <TableCell key={index} align="right">
-                      {row.Password}
-                    </TableCell>
-                    <TableCell key={index} align="right">
-                    <div className="flex justify-center">
-                    <div className="cursor-pointer text-green-600 ml-2" onClick={()=>editRecord(row)}>
-                    <EditIcon/>
-                    </div>
-                    <div className="cursor-pointer text-red-600 ml-4" onClick={()=>deleteRecord(row)}>
-                    <DeleteIcon/>
-                    </div>
-                    </div>
-                  </TableCell>
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={data.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+  //should be memoized or stable
+  const columns = useMemo(
+    () => [
+        {
+        accessorKey: "id", //access nested data with dot notation
+        header: "ID",
+        size: 50,
+      },
+      {
+        accessorKey: "name", //access nested data with dot notation
+        header: "Name",
+        size: 50,
+      },
+      {
+        accessorKey: "Email",
+        header: "Email",
+        size: 250,
+      },
+      {
+        accessorKey: "Password",
+        header: "Password",
+        size: 150,
+      },
+      
+    ],
+    []
   );
-}
+
+  const table = useMaterialReactTable({
+    columns,
+    data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+  });
+
+  return <MaterialReactTable table={table} />;
+};
+
+export default UsersList;
